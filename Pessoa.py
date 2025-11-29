329d4c1dc31bead61d12d4201ff153d0feab052e
Pedro Henrique Pereira <164574132+Phpedro02@users.noreply.github.com>
2025-11-29 09:05:57 -0300

    Add files via upload

---
+++ Pessoa.py
@@ -0,0 +1,11 @@
+class Exemplo:
+    #nome = ""
+    #nascimento = 0
+
+
+    def __init__(self, nome, anoNascimento):
+        self.nome = nome
+        self.nascimento = anoNascimento
+        self.atributo = 0
+       
+       
